0deb01f120501c876ebe3713ff1834ba2d7af67f
Joshnaacsha <joshnaacsha@gmail.com>
2025-03-31 17:25:10 +0000

    Updated code in dev branch

---
+++ project2.java
@@ -0,0 +1,17 @@
+public class SimpleWebPage {
+    public static void main(String[] args) {
+        String html = """
+            <html>
+                <head>
+                    <title>Simple Java Webpage 2</title>
+                </head>
+                <body>
+                    <h1>Welcome to My Simple Java Webpage 2</h1>
+                </body>
+            </html>
+        """;
+        
+        System.out.println(html);
+    }
+}
+
