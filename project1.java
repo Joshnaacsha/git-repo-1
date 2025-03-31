9461e22994ebfd0ee3a39c6b5f9c8d6d7a5bea03
joshnaacsha <joshnaacsha@gmail.com>
2025-03-31 17:05:01 +0000

    Initial commit

---
+++ project1.java
@@ -0,0 +1,16 @@
+public class SimpleWebPage {
+    public static void main(String[] args) {
+        String html = """
+            <html>
+                <head>
+                    <title>Simple Java Webpage</title>
+                </head>
+                <body>
+                    <h1>Welcome to My Simple Java Webpage</h1>
+                </body>
+            </html>
+        """;
+        
+        System.out.println(html);
+    }
+}
